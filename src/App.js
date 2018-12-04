d541c90e581c822e53dd64851e5e6f4f9048e796
lameck <lameck2@hotmail.com>
2018-12-04 12:20:24 -0300

    Adicionando coisas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Chat from "./Chat";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Batatinha</p>
+        <Chat />
       </div>
     );
   }
